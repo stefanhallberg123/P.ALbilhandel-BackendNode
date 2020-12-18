35e2a7268749ec0d0781cff2b0133e8c5912f89d
[stefanhallberg123]”git config --global user.name [stefanhallberg123]” <[stefanhalllberg@live.se]>
2020-12-18 17:12:19 +0100

    i think i did some changes

--- routes/messageform.js
+++ routes/messageform.js
@@ -8,6 +8,7 @@ router.post("/", async (req, res) => {
   const message = await new Message({
     // from input name in frontend
     regnumber: req.body.regnumber,
+    subject: req.body.subject,
     name: req.body.name,
     email: req.body.email,
     description: req.body.description,
@@ -16,29 +17,31 @@ router.post("/", async (req, res) => {
 });
 
 // get all the messages from admin
-router.get("/admin/start/messages", async (req, res) => {
+router.get("/admin/messages", async (req, res) => {
   const message = await Message.find({});
   res.send(message);
 });
 
 // get the info from Id to respond message
-router.get("/admin/message/add/:id", async (req, res) => {
+router.get("/admin/messages/add/:id", async (req, res) => {
   const id = await Message.findById({ _id: req.params.id });
   res.send(id);
 });
 
 // respond with email
-router.post("/admin/message/add", async (req, res) => {
+router.post("/admin/message/add/:id", async (req, res) => {
   const message = await new Message({
     regnumber: req.body.regnumber,
+    subject: req.body.subject,
     name: req.body.name,
     email: req.body.email,
     description: req.body.description,
   }).save();
+  console.log(message);
   res.send(message);
 });
 // delete message from id
-router.delete("/admin/message/delete/:id", async (req, res) => {
+router.delete("/admin/messages/:id", async (req, res) => {
   const id = await Message.findByIdAndDelete({
     _id: req.params.id,
   });
